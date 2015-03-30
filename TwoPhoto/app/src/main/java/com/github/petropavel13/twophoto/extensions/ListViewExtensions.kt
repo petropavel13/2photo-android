63ca7c2ced1812bb6fd54ef0f45d9b73b4868ff3
petropavel13 <mail4work0xFF@gmail.com>
2015-03-30 18:41:23 +0300

    make ListView extension more general

--- TwoPhoto/app/src/main/java/com/github/petropavel13/twophoto/extensions/ListViewExtensions.kt
+++ TwoPhoto/app/src/main/java/com/github/petropavel13/twophoto/extensions/ListViewExtensions.kt
@@ -1,14 +1,13 @@
 package com.github.petropavel13.twophoto.extensions
 
+import android.widget.AdapterView
 import android.widget.HeaderViewListAdapter
-import android.widget.ListAdapter
-import android.widget.ListView
 
 /**
  * Created by petropavel on 27/03/15.
  */
 
-fun ListView.getRealAdapter<T>(): T? {
+fun AdapterView<*>.getRealAdapter<T>(): T? {
     val adapter = getAdapter()
 
     if (adapter is HeaderViewListAdapter) {
