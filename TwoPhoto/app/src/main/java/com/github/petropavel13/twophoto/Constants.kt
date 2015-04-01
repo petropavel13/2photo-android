74a84cd00a72b27f07657b11eecf67678e5d5a79
petropavel13 <mail4work0xFF@gmail.com>
2015-04-01 14:33:10 +0300

    refactor

--- TwoPhoto/app/src/main/java/com/github/petropavel13/twophoto/Constants.kt
+++ TwoPhoto/app/src/main/java/com/github/petropavel13/twophoto/Constants.kt
@@ -6,9 +6,7 @@ import android.net.Uri
  * Created by petropavel on 25/03/15.
  */
 
-fun BuilderForApiUri(): Uri.Builder {
-    return Uri.Builder()
+fun BuilderForApiUri() = Uri.Builder()
             .scheme("http")
             .authority("198.49.66.155")
             .appendPath("v1")
-}
