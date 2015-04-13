926476043286c9a67dcfbf5c18f19e8abcb96b54
petropavel13 <mail4work0xFF@gmail.com>
2015-04-13 19:29:53 +0300

    100% kotlin

---
+++ TwoPhoto/app/src/androidTest/java/com/github/petropavel13/twophoto/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package com.github.petropavel13.twophoto
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+public class ApplicationTest : ApplicationTestCase<Application>(javaClass<Application>())
